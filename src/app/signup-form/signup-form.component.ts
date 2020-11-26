aeaf1b9dc69e4aff33e1adce3530fc8dd90fd062
Assylkhan <assylkhan.rakhatov@mail.ru>
2020-11-26 14:35:03 +0600

    add reactive sign up form

---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.scss']
+})
+export class SignupFormComponent {
+  signupForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    email: ['', Validators.required],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+  });
+
+  updateForm() {
+    this.signupForm.patchValue({
+      firstName: 'John',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  onSubmit() {
+    console.warn(this.signupForm.value);
+  }
+
+}
