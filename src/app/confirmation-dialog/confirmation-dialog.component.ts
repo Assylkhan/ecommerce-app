9ad7b48515045851021ff2b86c89737dc9d0fee7
Assylkhan <assylkhan.rakhatov@mail.ru>
2021-03-28 14:12:18 +0600

    adjust confirmation dialog

--- src/app/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
@@ -22,6 +22,7 @@ export class ConfirmationDialogComponent implements OnInit {
 }
 
 export interface DialogData {
-  animal: string;
   name: string;
+  description: string;
+
 }
